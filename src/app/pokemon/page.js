effd39449eeb589caee14ca1df0cc771e951014d
roblebaegaming <roblebaegaming@gmail.com>
2026-07-22 14:30:30 -0700

    Add Pokémon directory

---
+++ src/app/pokemon/page.js
@@ -0,0 +1,5 @@
+import PokemonDirectory from "../../components/PokemonDirectory";
+
+export const metadata = { title: "Pokémon | DraftCenter", description: "Explore Pokémon game data and DraftCenter community statistics." };
+
+export default function PokemonPage() { return <PokemonDirectory />; }
